983241cb3cfd3102f8e2cd20ae13d40b0017833f
kuang <cbkonline@yeah.net>
2021-06-08 15:11:00 +0800

    portal-移动端商城系统服务

---
+++ mall-portal/src/main/java/com/macro/mall/controller/UmsMemberController.java
@@ -0,0 +1,13 @@
+package com.macro.mall.controller;
+
+import com.macro.mall.common.api.CommonResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/sso")
+public class UmsMemberController {
+
+    
+}
